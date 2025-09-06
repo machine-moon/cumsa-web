82773be65985aad496582b65a9f2a5097ab6b0bf
SarahAbada <sabada628@gmail.com>
2025-09-06 14:14:56 -0400

    Update constants.ts
    Updated the information about the mosques

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -20,45 +20,44 @@ export const MOSQUES = [
   {
     name: "Ottawa Mosque (OMA)",
     address: "251 Northwestern Ave, Ottawa, ON",
-    lat: 45.4039,
-    lng: -75.7287,
+    lat: 45.4014535,
+    lng: -75.7413229,
     phone: "(613) 722-8763",
-    website: "https://www.omaottawa.org/",
+    website: "https://ottawamosque.ca/",
   },
   {
     name: "Masjid ar-Rahmah (AMA)",
     address: "1216 Hunt Club Rd, Ottawa, ON",
-    lat: 45.3454,
-    lng: -75.6457,
+    lat: 45.3517835,
+    lng: -75.6473151,
     phone: "(613) 523-9977",
     website: "https://www.amacanada.org/",
   },
   {
     name: "South Nepean Muslim Community (SNMC)",
     address: "3020 Woodroffe Ave, Nepean, ON",
-    lat: 45.2883,
-    lng: -75.7396,
+    lat: 45.28913497924805,
+    lng: -75.72648620605469,
     phone: "(613) 440-6300",
     website: "https://snmc.ca/",
   },
   {
     name: "Kanata Muslim Association (KMA)",
     address: "351 Sandhill Rd, Kanata, ON",
-    lat: 45.3023,
-    lng: -75.9167,
+    lat: 45.3560653,
+    lng: -75.9282921,
     phone: "(613) 973-5000",
     website: "https://kanatamuslims.ca/",
   },
   {
     name: "Centre Islamique de l’Outaouais (CIO)",
     address: "4 Rue Lois, Gatineau, QC",
-    lat: 45.4286,
-    lng: -75.7101,
-    phone: "(819) 777-0114",
-    website: "https://www.cio-qc.ca/",
+    lat: 45.4290273,
+    lng: -75.7321103,
+    phone: "(819) 776-1831",
+    website: "https://cio-oic.ca/",
   },
 ];
-
 // Forms and submissions
 export const FORMS = {
   newsletter:
